246a666a8937930ba334b9dbd1fa766deb4d8837
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-09-02 06:41:29 +0300

    3 was created structure of client projects with FC Login, Register

---
+++ 20240829_reactappauth.client/src/components/Register/Register.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { RegisterWrapper } from './Register.styled';
+
+interface RegisterProps {}
+
+const Register: FC<RegisterProps> = () => (
+ <RegisterWrapper>
+    Register Component
+ </RegisterWrapper>
+);
+
+export default Register;
